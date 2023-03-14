e9422723b15af7da1b4f4e5a5a42c576cf0603d8
Emin Kara <emin.kara@misli.com>
2023-03-14 10:37:24 +0300

    fix implementation

--- app/src/main/java/com/aek/kotlinmvvm/MainActivity.kt
+++ app/src/main/java/com/aek/kotlinmvvm/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.aek.kotlinmvvm
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.appcompat.app.AppCompatActivity
+import com.aek.kotlinmvvm.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
+        setContentView(binding.root)
     }
 }
